6492164ccbe1cbae8d401a03851e5db8beaecc8f
JordanR <2003jordanreyes@gmail.com>
2025-04-10 17:18:27 -0400

    Testing some changes

--- IES_FinalProject/main.c
+++ IES_FinalProject/main.c
@@ -42,7 +42,7 @@
 //          --|RST          XOUT|-
 //            |                 |
 //            |             P1.0|-->LED
-//
+// C:\Users\super\workspace_ccstheia\IES_Final_Project
 //  E. Chen
 //  Texas Instruments, Inc
 //  May 2018
@@ -53,30 +53,10 @@
 
 int main(void) {
 
-    volatile uint32_t i;
-
-    // Stop watchdog timer
-    WDT_A_hold(WDT_A_BASE);
-
-    // Set P1.0 to output direction
-    GPIO_setAsOutputPin(
-        GPIO_PORT_P1,
-        GPIO_PIN0
-        );
-
-    // Disable the GPIO power-on default high-impedance mode
-    // to activate previously configured port settings
-    PMM_unlockLPM5();
-
     while(1)
     {
-        // Toggle P1.0 output
-        GPIO_toggleOutputOnPin(
-            GPIO_PORT_P1,
-            GPIO_PIN0
-            );
-
-        // Delay
-        for(i=10000; i>0; i--);
+        // AMOGUS
+        // OMNIMAN
+        // "If he's invincible, why can I see him?"
     }
 }
