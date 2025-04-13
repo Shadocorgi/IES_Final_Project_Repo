e4917d2950a4f6d18943a4f618e71e8b34ada3f7
JordanR <2003jordanreyes@gmail.com>
2025-04-13 15:37:31 -0400

    Added RGB_init & setRGBLED functions

--- IES_FinalProject/main.c
+++ IES_FinalProject/main.c
@@ -1,11 +1,49 @@
 #include <driverlib.h>
 
+void system_init();
+
+
 int main(void) {
 
+    RGB_setColor(0, 0, 255); // Blue for idle system
     while(1)
     {
+        
         // AMOGUS
         // OMNIMAN
-        print("balls");
+        
+
     }
+}
+
+void system_init(){
+    power_on();
+    UART_init();
+    therm_Init();
+
+    pot_Init();
+    flame_Init();//thermocouple
+    heat_init(); //thermostat ("Call for heat")
+
+    RGB_init();
+    ignitor_init();
+    PilotValve_init();
+
+    MainValve_init();
+    I2C_init(); //If using any I2C peripherals
+    //-----------------------------------------\\
+   // Set initial system status
+    RGB_setColor(0, 0, 255); // Blue for idle system
+}
+
+void RGB_init(){
+    P6DIR |= BIT0 | BIT1 | BIT2;              // P6.0-P6.2 output
+    P6SEL0 |= BIT0 | BIT1 | BIT2;
+    P6SEL1 &= ~(BIT0 | BIT1 | BIT2);          // P6.0-P6.2 options select
+}
+
+void setRGBLED(char Red, char Green, char Blue){
+    TB3CCR3 = Red << 2;
+    TB3CCR2 = Green << 2;
+    TB3CCR1 = Blue << 2;
 }
