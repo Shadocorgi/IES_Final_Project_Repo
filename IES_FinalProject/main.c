8a5490a01fc9c4b004261c3438d99fb97ebc65dc
JordanR <2003jordanreyes@gmail.com>
2025-04-22 10:23:51 -0400

    Implemented pseudocode to create an outline of functions

--- IES_FinalProject/main.c
+++ IES_FinalProject/main.c
@@ -1,7 +1,8 @@
 // Final Project Code for IES
 // By: Jordan Reyes & Chelsea Small
 
-/*************************************************************************************** 
+//*************************************************************************************** 
+/*
 [COMPONENT PINS]
 - RGB LED
 	- Red: P6.0
@@ -17,35 +18,66 @@
 - Potentiometer - P1.5
 - Thermocouple - P1.3
 
-*///***************************************************************************************
-
+*/
+//***************************************************************************************
 #include <driverlib.h>
 #include <msp430.h>
+#include "Init Headers/flameInit.h"
 #include "systemInit.h"
+//==================================================================================================
+//Color Definitions
+#define BLUE 0, 0, 255
+#define RED 255, 0, 0
+#define GREEN 0, 255, 0
+#define YELLOW 255, 255, 0
+//STARTUP
+void heat_call();        // Thermostat requests heat
+void PilotValve_on();    // Open Gas Valve
+void ignitor_on();       // Ignite Pilot
+bool flame_detect();     // TRUE=flame present, FALSE=no flame
 
-//declare functions
-// void delay_cycles(); is implemented from msp430.h
-void RGB_setColor();
-
-void igniterOn();
-void igniterOff();
-
-void heatCall();
+//Flame Present
 void PilotValve_off();
-void MainValve_set(int valveposition);
+int pot_Read();
+int valvePosition = pot_Read();
+void MainValve_set(int valvePosition);
 
+//No Flame
+ignitor_off();
 
+//==================================================================================================
 int main(void) {
     // Initialization
     WDTCTL = WDTPW | WDTHOLD;  // Stop watchdog timer
     system_init();    
-    RGBsetColor(0, 0, 255); // Blue for idle system
-    
-    //Main Code
-    while(1)
-    {
-        
-    }
+    RGBsetColor(0, 0, 255);                      // STATE: Idle
+   if(heat_call()){                              // Thermostat requests heat
+				RGB_setColor(YELLOW);            // HEAT REQUEST STATE
+				PilotValve_On();                 // Open pilot gas valve
+				//delay_ms(200);                 // DELAY - Wait for gas to release
+				ignitor_on();                    // Ignite Pilot
+				//delay_ms(500);                 // DELAY - Wait for ignition to stabilize
+				
+				if(flame_Detect()){              // Ignited Pilot Success
+					RGB_setColor(GREEN);         // STATE: Heating
+					PilotValve_off();            // Turn off pilot lighter
+					MainValve_set(valvePosition);// Open main valve	
+				}
+				
+			else{
+				RGB_setColor(RED)                // STATE: Error (no flame)
+				PilotValve_off();                // Turn pilot gas off
+				ignitor_off();                   // Turn ignitor off
+			}
+		} else {                                 // No heat requested
+			RGB_setColor(BLUE);                  // STATE: Idle
+			MainValve_set(0)                     // Close Main Valve
+			PilotValve_off();                    // Ensure pilot gas valve is closed
+			ignitor_off();                       // Ensure ignitor is off
+		}
+		//delay_ms(1000);                        // DELAY - Wait b4 next loop cycle
+	}
+	return 0;
 }
 
 //==================================================================================================
@@ -56,12 +88,16 @@ void RGBsetColor(char Red, char Green, char Blue){
     TB3CCR2 = Green << 2;
     TB3CCR1 = Blue << 2;
 }//done
+void heatCall(){}
+bool flame_detect(){}
 
 void igniterOn(){}
 void igniterOff(){}
 
-void heatCall(){}
+void PilotValve_On(){}
 void PilotValve_off(){}
+
+int pot_Read(){}
 void MainValve_set(int valveposition){}
 
 //==================================================================================================
