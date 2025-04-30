5c38d82802e778909860ab804a583fab53743d93
JordanR <2003jordanreyes@gmail.com>
2025-04-30 14:48:38 -0400

    Fixed inconsistencies in function names and datatypes Reworked function names to camel-case formatting Changed heat_call and flame_detect to work off the same pin (P1.6) Moved system_init() to be the first function called in the main loop

--- IES_FinalProject/main.c
+++ IES_FinalProject/main.c
@@ -11,7 +11,7 @@
 - Red LED - P5.0
 
 - Pilot/Solenoid - 5.4 	
-- Call Heat - P3.0
+- Call Heat - P1.6 (uses thermistor)
 - Servo Motor - P2.1
 
 - Thermistor - P1.6
@@ -33,50 +33,53 @@
 #define YELLOW 255, 255, 0
 //STARTUP
 void RGB_setColor(uint8_t red, uint8_t green, uint8_t blue);
-void heat_call();        // Thermostat requests heat
-void PilotValve_on();    // Open Gas Valve
+bool heat_call();        // Thermostat requests heat
+void pilotValve_on();    // Open Gas Valve
 void ignitor_on();       // Ignite Pilot
 bool flame_detect();     // TRUE=flame present, FALSE=no flame
 
 //Flame Present
-void PilotValve_off();
-int pot_Read();
-int valvePosition = pot_Read();
-void MainValve_set(int valvePosition);
+void pilotValve_off();
+int pot_read();
+void mainValve_set(int valvePosition);
 
 //No Flame
-ignitor_off();
+
 
 //==================================================================================================
 int main(void) {
     // Initialization
     WDTCTL = WDTPW | WDTHOLD;  // Stop watchdog timer
-    
-    system_init();    
-    RGBsetColor(BLUE);                    // STATE: Idle
+	system_init();
+	ignitor_off();
+	int valvePosition = pot_read();
+	
+	
+	    
+    RGB_setColor(BLUE);                    // STATE: Idle
    if(heat_call()){                       // Thermostat requests a light
 		RGB_setColor(YELLOW);             // HEAT REQUEST STATE
-		PilotValve_On();                  // Open pilot gas valve
+		pilotValve_on();                  // Open pilot gas valve
 		//delay_ms(200);                  // DELAY - Wait for gas to release
 		ignitor_on();                     // Ignite Pilot
 		//delay_ms(500);                  // DELAY - Wait for ignition to stabilize
 				
-		if(flame_Detect()){               // Ignited Pilot Success (hi)
+		if(flame_detect()){               // Ignited Pilot Success (hi)
 			RGB_setColor(GREEN);          // STATE: Heating
-			PilotValve_off();             // Turn off pilot lighter
-			MainValve_set(30);            // Open main valve	
+			pilotValve_off();             // Turn off pilot lighter
+			mainValve_set(30);            // Open main valve	
 		}
 				
 		else{							  // (lo)
 			RGB_setColor(RED);            // STATE: Error (no flame)
-			PilotValve_off();             // Turn pilot gas off
+			pilotValve_off();             // Turn pilot gas off
 			ignitor_off();                // Turn ignitor off
 		}
 	} 
 	else {                                // No heat requested
 			RGB_setColor(BLUE);           // STATE: Idle
-			MainValve_set(0);             // Close Main Valve
-			PilotValve_off();             // Ensure pilot gas valve is closed
+			mainValve_set(0);             // Close Main Valve
+			pilotValve_off();             // Ensure pilot gas valve is closed
 			ignitor_off();                // Ensure ignitor is off
 	}
 	//delay_ms(1000);                     // DELAY - Wait b4 next loop cycle
@@ -84,36 +87,44 @@ int main(void) {
 }
 
 //==================================================================================================
+//Testing Area
+/*
+	RGBsetColor(BLUE);
+
+}
+*/
+//==================================================================================================
 //define functions
-
-void RGBsetColor(char Red, char Green, char Blue){
-    TB3CCR3 = Red << 2;
-    TB3CCR2 = Green << 2;
-    TB3CCR1 = Blue << 2;
-}//done
-
-bool heat_Call(){ 				// P3.0-Call Heat (Activate Button)
-    return ~(P1IN & BIT6); 		// Call for a light when there's no flame
+bool heat_call(){ 				// P1.6-Call Heat (Activate Button)
+    return ~(P1IN & BIT6); 		// Call for a light when there's no flame (uses thermistor)
 }
 bool flame_detect(){ 			// P1.6-Thermistor 
     return (P1IN & BIT6);		// TRUE if P1.6 returns a signal
 }
-int pot_Read(){ 				// P1.5-Potentiometer
-    return (P1OUT & BIT5);		// Read the altered values of the potentiometer
+int pot_read(){ 				// P1.5-Potentiometer
+    return (P1IN & BIT5);		// Read the altered values of the potentiometer
 }
-void ignitorOn(){ 				// P1.3-Thermocouple
+
+
+
+void RGB_setColor(uint8_t Red, uint8_t Green, uint8_t Blue){
+    TB3CCR3 = Red << 2;
+    TB3CCR2 = Green << 2;
+    TB3CCR1 = Blue << 2;
+}
+void ignitor_on(){ 				// P1.3-Thermocouple
     P1OUT |= BIT3;				// Output a signal at P1.3
 }
-void ignitorOff(){			    // P1.3-Thermocouple
+void ignitor_off(){			    // P1.3-Thermocouple
     P1OUT &= ~BIT3;				// Shut off signals outputting from P1.3
 }
-void PilotValve_On(){ 			// P5.4-Solenoid
+void pilotValve_on(){ 			// P5.4-Solenoid
     P5OUT |= BIT4;				// Output a signal at P5.4
 }
-void PilotValve_off(){ 			// P5.4-Solenoid
+void pilotValve_off(){ 			// P5.4-Solenoid
     P5OUT &= ~BIT4;				// Shut off signals outputting from P5.4
 }
-void MainValve_set(int valveposition){ // P2.1-Servo
+void mainValve_set(int valveposition){ // P2.1-Servo
     // Configure Timer B1
     TB1CCR0 = 330;                             // PWM Period/2 (sets ~50Hz at 32kHz ACLK)
     TB1CCTL2 = OUTMOD_7;                       // TB1.2 reset/set output mode
@@ -123,7 +134,7 @@ void MainValve_set(int valveposition){ // P2.1-Servo
 
 //==================================================================================================
 
-// All of these functions are transferred to headers
+// All of these initialization functions were transferred to headers
 // void RGB_init()        {} 
 // void UART_init()       {}  
 // void therm_Init()      {}
